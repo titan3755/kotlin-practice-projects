84052d059e165efcc66b4643a7d30d31d6039914
Titan <68808479+titan3755@users.noreply.github.com>
2025-02-26 20:07:52 +0600

    Update five.kt

--- five.kt
+++ five.kt
@@ -1,3 +1,7 @@
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.io.File
+
 fun main() {
     println("-- Contacts List --")
     println("Welcome to contact list app")
@@ -6,7 +10,8 @@ fun main() {
     println("1. Add contact")
     println("2. Remove contact")
     println("3. Edit contact")
-    println("4. Exit")
+    println("4. View contacts")
+    println("5. Exit")
     println("-- Enter the sl number associated with a function --")
     print("\n")
     print("Function: ")
@@ -15,6 +20,38 @@ fun main() {
         println("\nThe provided input is not valid, exiting ...")
         return
     }
+    // read file
+    val filePath = "contacts.txt"
+    val contacts = readContactsFromFile(filePath)
+    if (contacts == null) {
+        println("\nError reading contacts from file, creating new file ...")
+    }
+
 
 }
+
+fun readContactsFromFile(filePath: String): String? {
+    try {
+        val path = Paths.get(filePath)
+        return if (Files.exists(path) && Files.size(path) > 0) {
+            String(Files.readAllBytes(path))
+        } else {
+            null
+        }
+    } catch(_: Exception) {
+        return null
+    }
+}
+
+fun overwriteContactsToFile(filePath: String, content: String): Boolean {
+    try {
+        val path = Paths.get(filePath)
+        Files.write(path, content.toByteArray())
+        return true
+    } catch (_: Exception) {
+        return false
+    }
+}
+
+class Contact(val name: String, val number: String) {}
 
